1317ac8e0170d69ea16dffd1a35a9da941db068a
James Abels <jamesrabels@gmail.com>
2017-11-27 14:31:24 -0500

    Add permant test user to db

--- src/data/FakeDatabase.js
+++ src/data/FakeDatabase.js
@@ -3,7 +3,13 @@ import { generateUUID } from "../libs/crypto";
 class FakeDatabase{
     
         constructor() {
-            this.users = [];
+            this.users = [{
+                id: "66cf6fdc-d762-4766-a22a-93b24a8dfba6",
+                username: "SteveDave",
+                email: "steve@dave.com",
+                password: "$2a$12$fXyWZqvkpnglImS6cfl7IOQnqX8NtYd78pOQ7pcKgp1qOEL.xUfXG",
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoic3RldmVAZGF2ZS5jb20ifSwiaWF0IjoxNTExODA4Mjk2LCJleHAiOjE1NDMzNjU4OTZ9.XmHCm3WYzoAuLQ9cp4a37yX7pqBQyJhGjWGI59NxzfE"
+            }];
     
             this.todos = [];
         }
